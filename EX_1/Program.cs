7a6ed9bfd71b98119c3f05906092ec402dafcf4c
Lopushka <sv13yat@gmail.com>
2022-12-27 10:06:26 +0300

    Fin_ex_1_v_1

---
+++ EX_1/Program.cs
@@ -0,0 +1,15 @@
+﻿static void OutputCounter( int i)
+{
+    Console.Write(i + " ");
+    if (i<=1)
+        return;
+    
+    i--;
+    OutputCounter(i);
+}
+
+
+Console.Write("Give me a number: ");
+int numder = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+OutputCounter(numder);
